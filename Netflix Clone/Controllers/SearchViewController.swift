70163efcf6ad0a32ed6137591a34f833907977f1
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-07-19 02:36:25 +0300

    creating MainTabBarViewController

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Sasha Maksyutenko on 19.07.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Sasha Maksyutenko on 19.07.2023.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
