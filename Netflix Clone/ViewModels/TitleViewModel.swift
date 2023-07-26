2e55522541d30a53b132536bee41173b79abbc36
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-07-26 20:00:22 +0300

    Creating custom TableViewCell from the upcoming table & Creating TitleViewModel

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Sasha Maksyutenko on 26.07.2023.
+//
+
+import Foundation
+struct TitleViewModel{
+    let titleName:String
+    let posterURL:String
+}
